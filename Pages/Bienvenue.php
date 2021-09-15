bf8a80c4ef2631c1ee578504bca6e995f1b3b800
Théo Eudes <79459670+EdwardLe-cmd@users.noreply.github.com>
2021-09-15 20:31:48 +0200

    feat: ajout de nouveau contenu

--- Pages/Bienvenue.php
+++ Pages/Bienvenue.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Bienvenue sur mon site</title>
+    <link rel="stylesheet" type="text/css" href="../Style/CssAccueil.css">
   </head>
   <body>
-    test
+    <div class="Presentation">
+		<img class="PhotoProfil"  src="../Images/PhotoPerso.jpg">
+		<h1 class="TxtImportant">Bonjour et Bienvenue sur mon site Internet</h1>
+	</div>
   </body>
 </html>
