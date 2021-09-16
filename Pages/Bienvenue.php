323868c0f4a9c7e3f2c5d00693551948b5b10c77
Théo Eudes <79459670+EdwardLe-cmd@users.noreply.github.com>
2021-09-16 09:51:21 +0200

    feat: ajour de contenu sur la page de bienvenue

--- Pages/Bienvenue.php
+++ Pages/Bienvenue.php
@@ -10,5 +10,24 @@
     <img class="PhotoProfil"  src="../Images/PhotoPerso.jpg">
     <h1 class="TxtImportant">Bonjour et Bienvenue sur mon site Internet</h1>
   </div>
+  <div class="PresentationPerso">
+    <p>Si vous êtes sur ce site c'est que mon CV vous a plu, où c'est pour voir mes réalisations plus en profondeur, ce qui me fait plaisir merci !</p>
+    <p class="Intro">Bonjour, je suis Théo, j'ai 19 ans et je suis un étudiant en 2ème année de DUT informatique à Dijon</p>
+    <p>Je suis passionné d'informatique et du surout du Web</p>
+  </div>
+  <center><div class="container">
+    <div class="Competences">
+      <label>Competences</label>
+    </div>
+    <div class="Experiences">
+      <label>Experiences</label>
+    </div>
+    <div class="Formations">
+      <label>Formations</label>
+    </div>
+    <div class="Projets">
+      <label>Projets</label>
+    </div>
+  </div></center>
 </body>
 </html>
