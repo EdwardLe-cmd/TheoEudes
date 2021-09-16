6a182f17f2648ccda1deda4691314b4ac4c5760c
Théo Eudes <79459670+EdwardLe-cmd@users.noreply.github.com>
2021-09-16 08:36:57 +0200

    refactor: mauvaise indentation

--- Pages/Bienvenue.php
+++ Pages/Bienvenue.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Bienvenue sur mon site</title>
-    <link rel="stylesheet" type="text/css" href="../Style/CssAccueil.css">
-  </head>
-  <body>
-    <div class="Presentation">
-		<img class="PhotoProfil"  src="../Images/PhotoPerso.jpg">
-		<h1 class="TxtImportant">Bonjour et Bienvenue sur mon site Internet</h1>
-	</div>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Bienvenue sur mon site</title>
+  <link rel="stylesheet" type="text/css" href="../Style/CssAccueil.css">
+</head>
+<body>
+  <div class="Presentation">
+    <img class="PhotoProfil"  src="../Images/PhotoPerso.jpg">
+    <h1 class="TxtImportant">Bonjour et Bienvenue sur mon site Internet</h1>
+  </div>
+</body>
 </html>
